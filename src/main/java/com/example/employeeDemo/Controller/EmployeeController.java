dd0b8f01d37146a4d252ccfef34242dc9de1b6b9
Archanapudayan <atchanaudayan64534@gmail.com>
2023-01-19 12:54:45 +0530

    employeentry

--- src/main/java/com/example/employeeDemo/Controller/EmployeeController.java
+++ src/main/java/com/example/employeeDemo/Controller/EmployeeController.java
@@ -1,4 +1,14 @@
 package com.example.employeeDemo.Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
+    @GetMapping("/")
+    public String Homepage(){
+        return "Welcome to my welcome page";
+    }
 }
